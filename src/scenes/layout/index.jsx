b99b0926ff53ae68343a5fbf4059fff4b5b1dcb6
Alisson <alissonalves29@gmail.com>
2023-04-23 11:45:53 -0300

    added api to find users

--- src/scenes/layout/index.jsx
+++ src/scenes/layout/index.jsx
@@ -4,11 +4,15 @@ import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import NavBar from 'components/NavBar';
 import Sidebar from 'components/Sidebar';
+import { useGetUserQuery } from "state/api";
 
 const Layout = () => {
 
     const isNonMobile = useMediaQuery("(min-width: 600px)");
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const userId = useSelector((state) => state.global.userId);
+    const { data } = useGetUserQuery(userId);
+    console.log('data', data)
 
     return (
 
